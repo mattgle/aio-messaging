e244ee59f1624e9ba48bb65112976ed50875f799
Matt Grote <matiasgrote@gmail.com>
2024-10-23 15:21:33 -0300

    Kickoff

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import styles from './Header.module.scss';
+
+function Header() {
+  return <header className={styles.container}>TODO Header</header>;
+}
+
+export default Header;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import styles from './Home.module.scss';
+
+function Home() {
+  return (
+    <div className={styles.container}>
+      <p>Home Page</p>
+    </div>
+  );
+}
+
+export default Home;
